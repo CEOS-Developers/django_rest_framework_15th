5c56823848b0d0ca161d55534f1c2c683982b2ff
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-06 01:36:27 +0900

    get,post file list

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from api.serializers import *
+
+
+@csrf_exempt
+def file_list(request):
+    if request.method == 'GET':
+        files = File.objects.all()
+        serializer = FileSerializer(files, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = FileSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
