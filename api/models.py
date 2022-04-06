7f44518719a790611f535987ebaeb60cbb7819eb
김민준 <sanbonai06@naver.com>
2022-04-06 14:33:35 +0900

    Update modles.py

--- api/models.py
+++ api/models.py
@@ -4,41 +4,50 @@ from django.contrib.auth.models import User
 # 장고는 기본적으로 기본키 설정 (id = models.AutoField(primary_key=True))을 제공
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20, blank=True, null=True)
-    nickname = models.CharField(max_length=30)
-    profile_scripts = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(blank=True, null=True)
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성 시 현재 시간 자동 저장
     updated_at = models.DateTimeField(auto_now=True)  # 해당 레코드 갱신 시 현재 시간 자동 저장
     status = models.CharField(max_length=20, default='valid')
 
-
-class Following(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+    class Meta:
+        abstract=True
 
 
-class Follower(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, blank=True, null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    profile_scripts = models.TextField(blank=True, null=True)
+    profile_image = models.CharField(max_length=40, blank=True, null=True)
+
+    def __str__(self):
+        return self.nickname
 
 
-class Location(models.Model):
+class Following(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followingUser')
+
+    def __str__(self):
+        return self.user_id
+
+
+class Follower(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followerUser')
+
+    def __str__(self):
+        return self.user_id
+
+
+class Location(BaseModel):
     name = models.TextField()
     latitude = models.TextField()
     longitude = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+
+    def __str__(self):
+        return self.name
 
 
-class Post(models.Model):
+class Post(BaseModel):
     POST_TYPE = (
         ("po", "Post"),
         ("re", "Reels")
@@ -47,56 +56,50 @@ class Post(models.Model):
     script = models.TextField(blank=True, null=True)
     type = models.CharField(max_length=20, choices=POST_TYPE)
     location = models.ForeignKey(Location, on_delete=models.PROTECT, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+
+    def __str__(self):
+        return self.type
 
 
-class Image(models.Model):
+class Image(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     url = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
 
 
-class Movie(models.Model):
+class Movie(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     url = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
 
 
-class Liking(models.Model):
+class Liking(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+
+    def __str__(self):
+        return self.user_id
 
 
-class Comment(models.Model):
+class Comment(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     script = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+
+    def __str__(self):
+        return self.user_id
 
 
-class Story(models.Model):
+class Story(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='valid')
+
+    def __str__(self):
+        return self.user_id
 
 
-class ViewingStory(models.Model):
+class ViewingStory(BaseModel):
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     liking = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default="valid")
+
+    def __str__(self):
+        return self.story_id
