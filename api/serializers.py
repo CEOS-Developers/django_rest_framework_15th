1fb0449d6d79db92b69f17d959dc2306c87c21c6
gkim <kimkm0419@naver.com>
2022-05-01 20:05:33 +0900

    Feat : Add LikeSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Post
+from .models import *
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['created_at']
 
 class PostSerializer(serializers.ModelSerializer):
+    likes = LikeSerializer(many=True, read_only=True)
+
     class Meta:
         model = Post  # 사용할 모델
-        fields = ['id', 'user', 'content', 'created_at', 'modified_at']  # 사용할 모델의 필드
+        fields = ['user', 'content', 'created_at', 'modified_at', 'likes']  # 사용할 모델의 필드
