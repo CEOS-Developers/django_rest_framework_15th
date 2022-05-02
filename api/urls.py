127ee928c7bedc8016660cc2675921e1babda86f
gkim <kimkm0419@naver.com>
2022-05-03 05:38:24 +0900

    Fix : Revise urlpatterns for CBV

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('posts/', views.post_list)
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
