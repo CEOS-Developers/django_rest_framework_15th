ea7c71b7b99b6a84b05318be28c28bad1ed9ef5b
Soyun Won <soyun9902@ewhain.net>
2022-05-06 04:27:37 +0900

    특정 데이터를 가져오는 API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.serializers import *
@@ -18,4 +19,15 @@ class PostList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        posts = self.get_object(pk)
+        serializer = PostSerializer(posts)
+        return Response(serializer.data)
 
