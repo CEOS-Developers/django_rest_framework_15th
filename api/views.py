8c2de94264079fe00c05f7c6245f8d10f0970fd3
Soyun Won <soyun9902@ewhain.net>
2022-05-06 04:23:48 +0900

    새로운 데이터를 create하도록 요청하는 API

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
 from api.serializers import *
 from api.models import *
+from rest_framework import status
 
 
 class PostList(APIView):
@@ -10,5 +10,12 @@ class PostList(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
