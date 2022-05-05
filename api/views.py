e3f2e0556b6793d8b1e99297abcb48c8a2e4ca34
Soyun Won <soyun9902@ewhain.net>
2022-05-06 04:28:50 +0900

    특정 데이터를 업데이트하는 API

--- api/views.py
+++ api/views.py
@@ -31,3 +31,11 @@ class PostDetail(APIView):
         serializer = PostSerializer(posts)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        posts = self.get_object(pk)
+        serializer = PostSerializer(posts, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
