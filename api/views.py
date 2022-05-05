ef09698571f023d283c24d671678c3c87926bad1
Soyun Won <soyun9902@ewhain.net>
2022-05-06 04:19:08 +0900

    모든 데이터를 가져오는 api

--- api/views.py
+++ api/views.py
@@ -1,21 +1,14 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from .models import *
-from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from api.serializers import *
+from api.models import *
 
 
-@csrf_exempt
-def post_list(request):
-    if request.method == 'GET':
+class PostList(APIView):
+    def get(self, request, format=None):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+
