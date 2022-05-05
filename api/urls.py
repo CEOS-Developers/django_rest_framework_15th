fbdfa99db8aa8c1f9a3b80bb411760c41cd9bee0
jihoon <jihoon806>
2022-05-06 02:42:48 +0900

    drf2_cbv_end

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('api/comments', views.comment_api),
-    path('api/likes', views.like_api),
-    path('api/medias', views.media_api),
-    path('api/posts', views.post_api),
-    path('api/profiles', views.profile_api)
+    path('api/comments', views.CommentList.as_view()),
+    path('api/comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('api/likes', views.LikeList.as_view()),
+    path('api/likes/<int:pk>/', views.LikeList.as_view()),
+    path('api/medias', views.MediaList.as_view()),
+    path('api/medias/<int:pk>/', views.MediaDetail.as_view()),
+    path('api/posts', views.PostList.as_view()),
+    path('api/posts/<int:pk>/', views.PostDetail.as_view()),
+    path('api/profiles', views.ProfileList.as_view()),
+    path('api/profiles/<int:pk>/', views.ProfileDetail.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from .serializers import *
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
 from rest_framework import status
 
 
@@ -12,7 +11,7 @@ class ProfileList(APIView):
     """
     def get(self, request, format=None):
         queryset = Profile.objects.all()
-        serializer = PostSerializer(queryset, many=True);
+        serializer = ProfileSerializer(queryset, many=True);
         return Response(serializer.data)
     """
     사용자(프로필) 생성 /api/profiles POST
@@ -31,11 +30,11 @@ class ProfileDetail(APIView):
     """
     def get_one(self,pk):
         try:
-            return Post.objects.get(pk=pk)
+            return Profile.objects.get(pk=pk)
         except Profile.DoesNotExist:
             raise Http404
     """
-    특정 Profile 조회 ./api/profiles/{pk} GET
+    특정 Profile 조회 ./api/profiles/{pk}/ GET
     """
     def get(self, request, pk):
         profile = self.get_one(pk);
@@ -43,27 +42,59 @@ class ProfileDetail(APIView):
         return Response(serializer.data)
     """
     특정 profile 수정 -> put(완전대체), patch(수정), post(patch 불가 사항에 대해 -> 재생성 개념?)
-    일단 put 사용 ./api/profiles/{pk} PUT
+    일단 put 사용 ./api/profiles/{pk}/ PUT
     """
     def put(self, request, pk):
         profile = self.get_one(pk)
-        serializer = PostSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     """
-    Profile 하나 삭제. /api/profiles/{pk} DELETE
+    Profile 하나 삭제. /api/profiles/{pk}/ DELETE
     """
     def delete(self, request, pk, format=None):
         profile = self.get_one(pk)
-        profile.delete()
+        profile.delete();
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class PostList(APIView):
+  def get(self):
+      return ;
 
 
+class PostDetail(APIView):
+    def get_one(self):
+        return;
 
 
+class LikeList(APIView):
+    def get(self):
+        return ;
 
 
+class LikeDetail(APIView):
+    def get_one(self):
+        return ;
+
+
+class CommentList(APIView):
+    def get(self):
+        return;
+
+
+class CommentDetail(APIView):
+    def get_one(self):
+        return
+
+
+class MediaList(APIView):
+    def get(self):
+        return
+
+
+class MediaDetail(APIView):
+    def get_one(self):
+        return
