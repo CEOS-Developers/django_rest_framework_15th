68ad6d907f8f6325780c56ae5aff2d437a4d003a
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-05-06 18:05:46 +0900

    feedback 반영

--- api/serializers.py
+++ api/serializers.py
@@ -9,7 +9,7 @@ class FileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = File
-        fields = '__all__'
+        fields = ['id', 'post_content', 'type', 'url']
 
     def get_post_content(self, obj):
         return obj.post.content
