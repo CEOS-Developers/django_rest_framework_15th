9112bdfc3e606825adf5f98f7ef7f4090997e39c
yesjjin99 <yesjjin99@naver.com>
2022-05-06 11:37:35 +0900

    Add api views with CBV

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,13 @@ from api import views
 
 
 urlpatterns = [
-	path('posts/', views.post_list),
+	# post
+	path('posts/', views.PostList.as_view()),
+	path('posts/<int:pk>/', views.PostDetail.as_view()),
+	# profile
+	path('profiles/', views.ProfileList.as_view()),
+	path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+	# comment
+	path('comments/', views.CommentList.as_view()),
+	path('comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
