f5f1dba1288d946c3728fcf492ce699d1c0be4db
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-05-10 20:36:08 +0900

    filtering

--- api/views.py
+++ api/views.py
@@ -1,16 +1,29 @@
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from api.serializers import *
+from django_filters.rest_framework import FilterSet, filters, DjangoFilterBackend
 
 
-class FileViewSet(viewsets.ModelViewSet):
+class FileFilter(FilterSet):
+    url = filters.CharFilter(field_name='url', lookup_expr='icontains')
+    type = filters.CharFilter(method='filter_by_type')
+
+    class Meta:
+        model = File
+        fields = ['url']
+
+    def filter_by_type(self, queryset, name, value):
+        filtered_queryset = queryset.filter(type=value)
+        return filtered_queryset
+
+
+class FileViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = FileSerializer
     queryset = File.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = FileFilter
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
