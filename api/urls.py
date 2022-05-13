6aa2d044f0704d78abe0047a3c25dd911766d298
yesjjin99 <yesjjin99@naver.com>
2022-05-13 17:15:56 +0900

    Refactor APIView to ViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,10 @@
-from django.urls import path
-from api import views
+from rest_framework import routers
+from api.views import *
 
 
-urlpatterns = [
-	# post
-	path('posts/', views.PostList.as_view()),
-	path('posts/<int:pk>/', views.PostDetail.as_view()),
-	# profile
-	path('profiles/', views.ProfileList.as_view()),
-	path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
-	# comment
-	path('comments/', views.CommentList.as_view()),
-	path('comments/<int:pk>/', views.CommentDetail.as_view()),
-]
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,102 +1,17 @@
 from api.serializers import *
-from api.models import *
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.status import *
-from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins
 
 
-class PostList(APIView):
-	def get(self, request, format=None):
-		posts = Post.objects.all()
-		serializer = PostSerializer(posts, many=True)
-		return Response(serializer.data)
-
-	def post(self, request, format=None):
-		serializer = PostSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=HTTP_201_CREATED)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+class PostViewSet(viewsets.ModelViewSet):
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
 
 
-class PostDetail(APIView):
-	def get_object(self, pk):
-		return get_object_or_404(Post, pk=pk)
-
-	def get(self, request, pk, format=None):
-		post = self.get_object(pk)
-		serializer = PostSerializer(post)
-		return Response(serializer.data)
-
-	def put(self, request, pk, format=None):
-		post = self.get_object(pk)
-		serializer = PostSerializer(post, data=request.data) # put/patch
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-	def delete(self, request, pk, format=None):
-		post = self.get_object(pk)
-		post.delete()
-		return Response(status=HTTP_204_NO_CONTENT)
+class ProfileViewSet(viewsets.ModelViewSet):
+	serializer_class = ProfileSerializer
+	queryset = Profile.objects.all()
 
 
-class ProfileList(APIView):
-	def post(self, request, format=None):
-		serializer = ProfileSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=HTTP_201_CREATED)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-class ProfileDetail(APIView):
-	def get_object(self, pk):
-		return get_object_or_404(Profile, pk=pk)
-
-	def get(self, request, pk, format=None):
-		profile = self.get_object(pk)
-		serializer = ProfileSerializer(profile)
-		return Response(serializer.data)
-
-	def put(self, request, pk, format=None):
-		profile = self.get_object(pk)
-		serializer = ProfileSerializer(profile, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-class CommentList(APIView):
-	def get(self, request, format=None):
-		comments = Comment.objects.all()
-		serializer = CommentSerializer(comments, many=True)
-		return Response(serializer.data)
-
-	def post(self, request, format=None):
-		serializer = CommentSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=HTTP_201_CREATED)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-class CommentDetail(APIView):
-	def get_object(self, pk):
-		return get_object_or_404(Comment, pk=pk)
-
-	def put(self, request, pk, format=None):
-		comment = self.get_object(pk)
-		serializer = CommentSerializer(comment, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-	def delete(self, request, pk, format=None):
-		comment = self.get_object(pk)
-		comment.delete()
-		return Response(status=HTTP_204_NO_CONTENT)
+class CommentViewSet(viewsets.ModelViewSet):
+	serializer_class = CommentSerializer
+	queryset = Comment.objects.all()
