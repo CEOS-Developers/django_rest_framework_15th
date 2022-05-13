430f3200164caf9a37d9c9e9356d63e19c343761
yesjjin99 <yesjjin99@naver.com>
2022-05-13 17:23:19 +0900

    Add filtering

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from api.views import *
 
 
 router = routers.DefaultRouter()
+router.register(r'files', FileViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'profiles', ProfileViewSet)
 router.register(r'comments', CommentViewSet)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,26 @@
 from api.serializers import *
 from rest_framework import viewsets, mixins
+from django_filters.rest_framework import FilterSet, filters, DjangoFilterBackend
+
+
+class FileFilter(FilterSet):
+	path = filters.CharFilter(field_name='path')
+	type = filters.NumberFilter(method='filter_type')
+
+	class Meta:
+		model = File
+		fields = ['path']
+
+	def filter_type(self, queryset, name, value):
+		filtered_queryset = queryset.filter(type=value)
+		return filtered_queryset
+
+
+class FileViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+	serializer_class = FileSerializer
+	queryset = File.objects.all()
+	filter_backends = [DjangoFilterBackend]
+	filterset_class = FileFilter
 
 
 class PostViewSet(viewsets.ModelViewSet):
