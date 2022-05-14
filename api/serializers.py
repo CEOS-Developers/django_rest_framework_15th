527b00b25a12408add925a10849a8a9bcfd80021
gkim <kimkm0419@naver.com>
2022-05-15 02:10:43 +0900

    Fix : Add fields to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,11 @@ from .models import *
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ['created_at']
+        fields = ['post', 'user', 'created_at']
 
 class PostSerializer(serializers.ModelSerializer):
     likes = LikeSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post  # 사용할 모델
-        fields = ['user', 'content', 'created_at', 'modified_at', 'likes']  # 사용할 모델의 필드
+        fields = ['id', 'user', 'content', 'created_at', 'modified_at', 'likes']  # 사용할 모델의 필드
