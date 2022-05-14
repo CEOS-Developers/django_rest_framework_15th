b8cdae6e3c8e6d08cf10e399baafa0652e28fb14
cse_122 <zinc_from2020@naver.com>
2022-05-14 14:21:02 +0900

    viewset -router

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from api import views
+from rest_framework import routers
+from .views import *
 
+# Viewset
+
+router = routers.DefaultRouter()
+router.register(r'Profile', ProfileViewset)
+router.register(r'Post', PostViewset)
+
+urlpatterns = router.urls
+
+# APIView
+'''
 urlpatterns = [
     path('api/profiles', views.ProfileList.as_view()),
     path('api/profiles/<int:pk>', views.ProfileDetail.as_view()),
     path('api/posts', views.PostList.as_view()),
     path('api/posts/<int:pk>', views.PostDetail.as_view())
 ]
+'''
+
