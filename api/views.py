cbd9deca939b528bdd1b6d354ca0ad4c59a195e7
gkim <kimkm0419@naver.com>
2022-05-15 03:02:13 +0900

    Feat : Add filter_is_recently_modified

--- api/views.py
+++ api/views.py
@@ -3,14 +3,21 @@ from api.serializers import *
 from rest_framework import viewsets
 from django_filters.rest_framework import FilterSet, filters
 from django_filters.rest_framework import DjangoFilterBackend
+import datetime
 
 
 class PostFilter(FilterSet):
 	user = filters.NumberFilter(field_name='user')
+	is_recently_modified = filters.BooleanFilter(method='filter_is_recently_modified')
 
 	class Meta:
 		model = Post
 		fields = '__all__'
+
+	def filter_is_recently_modified(self, queryset, name, value):
+		end_date = datetime.datetime.today().replace(minute=0, second=0, microsecond=0)
+		start_date = end_date - datetime.timedelta(hours=6)	# 최근 6시간 이내에 수정된 포스트
+		return queryset.filter(modified_at__range=(start_date, end_date))
 
 class PostViewSet(viewsets.ModelViewSet):
 	serializer_class = PostSerializer
