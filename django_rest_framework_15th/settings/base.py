304e18791ff1a69e7cf0db46e4c5b1c9e2b935ca
cse_122 <zinc_from2020@naver.com>
2022-05-14 14:50:14 +0900

    setting REST_FRAMEWORK

--- django_rest_framework_15th/settings/base.py
+++ django_rest_framework_15th/settings/base.py
@@ -118,3 +118,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
